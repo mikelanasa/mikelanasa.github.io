f2bb445f91de98e47d7b670fe6d3f99d6db6d107
mikelanasa <123777841+mikelanasa@users.noreply.github.com>
2023-10-08 15:36:46 -0400

    Delete landing.php

--- landing.php
+++
@@ -1,9 +0,0 @@
-<?php
-// landing.php - Verify that program was called by Landing Page, if not transfer to Landing Page
-
-// $landing is SET in Landing Page, if not set, then program was called directly and send back to home page
-  if (!isset($landing)) {
-    header('Location: website.php'); 
-	exit;
-	}
-?>
